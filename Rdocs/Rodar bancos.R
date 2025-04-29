41c490cbe2c1cd439cc824099c73cfe982d94567
hugosaigg <hugo.saigg@gmail.com>
2025-04-29 17:09:20 -0300

    att

---
+++ Rdocs/Rodar bancos.R
@@ -0,0 +1,9 @@
+pop_cad_2023 <- read_excel('DadosAtividadesePop/grupos 1 e 2 População Cadastrada/pop_cadastrada_2023.xls')
+
+pse_2023 <- read.xlsx('DadosAtividadesePop/Saúde na Escola/Produção do PSE/producao_pse_2023.xlsx', startRow = 10)
+
+prom_saude_2023 <- read.xlsx('DadosAtividadesePop/grupo 2 Práticas para a Saúde/praticas2023.xlsx', startRow = 10)
+
+temas_saude_2023 <- read.xlsx('DadosAtividadesePop/grupo 1 Temas para a Saúde/temas2023.xlsx', startRow = 10)
+
+escolas_adesao_2023_2024 <- read_excel("DadosAtividadesePop/Saúde na Escola/Escolas com Adesão - Denominador/escolas_adesao_2023_2024.xlsx", skip = 5)
